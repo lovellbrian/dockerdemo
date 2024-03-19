4828173e6ab109744ba3f310547c56ae14bb9642
Brian Lovell <lovell@itee.uq.edu.au>
2024-03-19 11:09:50 +1000

    Add docker

--- Dockerdemo.sh
+++ Dockerdemo.sh
@@ -1,1 +1,6 @@
 # This is shell script containing the commands to build a container in GO
+
+# Based on  https://www.youtube.com/watch?v=8fi7uSYlOdc
+# and       https://www.youtube.com/watch?v=Utf-A4rODH8
+
+# Step 1
