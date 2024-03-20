0574974928138d12f7a44d6b7335f30131890aa4
Brian Lovell <lovell.brian@gmail.com>
2024-03-20 02:10:45 +0000

    step1

--- Dockerdemo.sh
+++ Dockerdemo.sh
@@ -3,7 +3,8 @@
 # Based on  https://www.youtube.com/watch?v=8fi7uSYlOdc
 # and       https://www.youtube.com/watch?v=Utf-A4rODH8
 
-# Step 1
+# Show first 3 slides up to run docker
+
 docker run --rm -it ubuntu /bin/bash
 hostname # in container
 hostname # in host
@@ -12,45 +13,11 @@ ps  # in container
 ps  # in host
 exit # in container
 
-# Want to recreate 'docker run' in Go
-# show slide on namespaces - restricting the view
-
-# create main.go
-go run main.go run echo "Hello ELEC4630"
-
-#Edit to make it run functions. Connect Stdout etc
-
-go run main.go run /bin/bash
-
-# Now need to containerize
-
-ls -l /proc/self/exe
-ls -l /proc/self/  # multiple times
-
-sleep 100 # in container
-
-ps -C sleep # on host
-ls -l /proc/8941
-ls -l /proc/8941/root
-
-ls /proc # in container
-ps
-# get Error, do this: mount -t proc proc /proc
-
-# Run again
-ps
-mount
-
-sleep 100 # in container
-ps -C sleep # on host
-cat /proc/8029/mounts
-# get  /proc proc rw,relatime 0 0
-
-# cGroups to restrict resources
-
-cd /sys/fs/cgroup # on host
-ls
-cd memory
-cat memory.limit_in_bytes
-
+# Show namespaces slide
+# Edit main.go
+# Step1
+# Open bash terminal, detach
+sudo -i
+cd /workspaces/dockerdemo
+go run main.go run "Hello ELEC4630"
 
--- main.go
+++ main.go
@@ -1,60 +1,23 @@
 package main
 
-import (
-	"fmt"
-	"os"
-	"os/exec"
-	"syscall"
-)
+import "os"
+import "fmt"
 
-// docker     run image <cmd> <params>
-// go run main.go run     <cmd> <params>
+// docker        run image <cmd> <params>
+// go run main.go          <cmd> <params>
 
 func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
-	case "child":
-		child()
-
+	
 	default:
 		panic("bad command")
 	}
 }
 
-func run() {
-	fmt.Printf("running %v as %d\n", os.Args[2:], os.Getpid())
-
-	
-	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
-		Unshareflags: syscall.CLONE_NEWNS,
-	}
-	cmd.Run()
-
-}
-
-func child() {
-	fmt.Printf("running %v as %d\n", os.Args[2:], os.Getpid())
-
-	syscall.Sethostname([]byte("container"))
-	syscall.Chroot("/host-home-folder/ubuntu-fs")
-	syscall.Chdir("/")
-	syscall.Mount("proc", "proc", "proc", 0, "")
-	
-	cmd := exec.Command(os.Args[2], os.Args[3:]...)
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	
-	cmd.Run()
-
-	syscall.Unmount("/proc", 0)
-
+func run(){
+	fmt.Printf("Running %v\n", os.Args[2:])
 }
 
 func must(err error) {
