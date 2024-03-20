8ff1c1a19dc65e40fd46135aa0b6bbc7f8b29520
Brian Lovell <lovell@itee.uq.edu.au>
2024-03-20 22:07:52 +1000

    Edit

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
-import "os"
-import "fmt"
-import "os/exec"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
 
 // docker        run image <cmd> <params>
 // go run main.go          <cmd> <params>
@@ -11,13 +13,13 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
-	
+
 	default:
 		panic("bad command")
 	}
 }
 
-func run(){
+func run() {
 	fmt.Printf("Running %v\n", os.Args[2:])
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
