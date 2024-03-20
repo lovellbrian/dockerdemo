d13fb2a93ef0f28de3179da72b1327181f393b55
Brian Lovell <lovell.brian@gmail.com>
2024-03-21 01:18:19 +1000

    Edit

--- Dockerdemo.sh
+++ Dockerdemo.sh
@@ -3,7 +3,7 @@
 # Based on  https://www.youtube.com/watch?v=8fi7uSYlOdc
 # and       https://www.youtube.com/watch?v=Utf-A4rODH8
 
-# Step 1
+# main branch
 docker run --rm -it ubuntu /bin/bash
 hostname # in container
 hostname # in host
@@ -14,43 +14,9 @@ exit # in container
 
 # Want to recreate 'docker run' in Go
 # show slide on namespaces - restricting the view
+# Create main.go adding func(run)
 
-# create main.go
+# step1
 go run main.go run echo "Hello ELEC4630"
-
-#Edit to make it run functions. Connect Stdout etc
-
-go run main.go run /bin/bash
-
-# Now need to containerize
-
-ls -l /proc/self/exe
-ls -l /proc/self/  # multiple times
-
-sleep 100 # in container
-
-ps -C sleep # on host
-ls -l /proc/8941
-ls -l /proc/8941/root
-
-ls /proc # in container
-ps
-# get Error, do this: mount -t proc proc /proc
-
-# Run again
-ps
-mount
-
-sleep 100 # in container
-ps -C sleep # on host
-cat /proc/8029/mounts
-# get  /proc proc rw,relatime 0 0
-
-# cGroups to restrict resources
-
-cd /sys/fs/cgroup # on host
-ls
-cd memory
-cat memory.limit_in_bytes
 
 
