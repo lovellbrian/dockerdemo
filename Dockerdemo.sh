959b7935e43f843b397bb83e32bf74dd67a42341
Brian Lovell <lovell.brian@gmail.com>
2024-03-21 02:04:19 +1000

    Edit

--- Dockerdemo.sh
+++ Dockerdemo.sh
@@ -2,9 +2,8 @@
 
 # Based on  https://www.youtube.com/watch?v=8fi7uSYlOdc
 # and       https://www.youtube.com/watch?v=Utf-A4rODH8
-# main (select main branch, then step1, step2 ...)
-# main (select main branch, then step1, step2 ...)
 
+# main (select main branch, then step1, step2 ...)
 # Show first 3 slides up to run docker
 
 docker run --rm -it ubuntu /bin/bash
