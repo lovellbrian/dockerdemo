06bba94ee15a923c9e57f5a1ef8e644d48675fcb
Brian Lovell <lovell.brian@gmail.com>
2024-03-20 02:49:03 +0000

    edit

--- main.go
+++ main.go
@@ -14,6 +14,8 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
+	case "child":
+		child()
 	
 	default:
 		panic("bad command")
@@ -23,7 +25,8 @@ func main() {
 func run(){
 	fmt.Printf("Running %v\n", os.Args[2:])
 
-	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	cmd.Stdin = os.Stdin
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -32,7 +35,20 @@ func run(){
 	}
 	cmd.Run()
 
-	syscall.Sethostname([]byte("container")) # this will not work
+}
+
+func child(){
+	fmt.Printf("Running %v\n", os.Args[2:])
+	syscall.Sethostname([]byte("container"))
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	
+	cmd.Run()
+
+	syscall.Sethostname([]byte("container"))
 }
 
 func must(err error) {
