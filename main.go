26d77e7898c47de5e3bd8ee1fc7cd722ba55f04f
Brian Lovell <lovell.brian@gmail.com>
2024-03-21 11:00:42 +1000

    Changed chroot folder

--- main.go
+++ main.go
@@ -16,13 +16,13 @@ func main() {
 		run()
 	case "child":
 		child()
-	
+
 	default:
 		panic("bad command")
 	}
 }
 
-func run(){
+func run() {
 	fmt.Printf("running %v as %d\n", os.Args[2:], os.Getpid())
 
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
@@ -31,27 +31,27 @@ func run(){
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
 	}
 	cmd.Run()
 
 }
 
-func child(){
+func child() {
 	fmt.Printf("running %v as %d\n", os.Args[2:], os.Getpid())
 
 	syscall.Sethostname([]byte("container"))
-	syscall.Chroot("/host-home-folder/ubuntu-fs")
+	syscall.Chroot("/home/vscode/ubuntu-fs")
 	syscall.Chdir("/")
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	
+
 	cmd.Run()
 
-	}
+}
 
 func must(err error) {
 	if err != nil {
