0f7f7dd27f69853e04ceb05cf21c61ef74c4e7d5
Brian Lovell <lovell.brian@gmail.com>
2024-03-21 11:14:47 +1000

    CRLF

--- main.go
+++ main.go
@@ -16,13 +16,13 @@ func main() {
 		run()
 	case "child":
 		child()
-	
+
 	default:
 		panic("bad command")
 	}
 }
 
-func run(){
+func run() {
 	fmt.Printf("Running %v\n", os.Args[2:])
 
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
@@ -31,13 +31,13 @@ func run(){
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags:   syscall.CLONE_NEWUTS,
+		Cloneflags: syscall.CLONE_NEWUTS,
 	}
 	cmd.Run()
 
 }
 
-func child(){
+func child() {
 	fmt.Printf("Running %v\n", os.Args[2:])
 	syscall.Sethostname([]byte("container"))
 
@@ -45,10 +45,10 @@ func child(){
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	
+
 	cmd.Run()
 
-	}
+}
 
 func must(err error) {
 	if err != nil {
